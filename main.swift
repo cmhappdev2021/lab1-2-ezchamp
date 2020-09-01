6e91a9f15882ac5fce9a4410d6d964a988e6e63f
ezchamp <emile269@gmail.com>
2020-09-01 18:55:55 +0000

    Created two print statements

--- main.swift
+++ main.swift
@@ -18,6 +18,8 @@ let goalSteps = 10000
 
 // Use two print functions to print two separate lines to the console. The first line should say "Your step goal for the day is:", and the second line should print the value of goalSteps by referencing your constant.
 
+print("Your step goal for the day is:")
+print(goalSteps)
 
 // Declare a variable schooling and set it to the number of years of school that you have completed. Print schooling to the console.
 
