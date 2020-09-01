5d5215569291fabb8b104ba3fb49060b95536384
ezchamp <emile269@gmail.com>
2020-09-01 18:52:47 +0000

    Created a constant named goalSteps and assigned it a value

--- main.swift
+++ main.swift
@@ -14,7 +14,7 @@ print("The code does not compile because once a constant's value has been set, i
 // Note: These exercises reinforce Swift concepts in the context of a fitness tracking app.
 // Your fitness tracking app needs to know goal number of steps per day. Create a constant goalSteps and set it to 10000.
 
-
+let goalSteps = 10000
 
 // Use two print functions to print two separate lines to the console. The first line should say "Your step goal for the day is:", and the second line should print the value of goalSteps by referencing your constant.
 
