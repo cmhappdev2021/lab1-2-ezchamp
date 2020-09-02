e07fc85da0a139e2b6c628527c4101976a618564
ezchamp <emile269@gmail.com>
2020-09-02 18:04:17 +0000

    Made a variable. Printed the variable.

--- main.swift
+++ main.swift
@@ -37,6 +37,8 @@ print("The code above complies because variables are able to change in value whe
 
 // Create a variable called steps that will keep track of the number of steps you take throughout the day. Set its initial value to 0 to represent the step count first thing in the morning. Print steps to the console.
 
+var steps = 0
+print(steps)
 
 // Now assume the tracker has been keeping track of steps all morning, and you want to show the user the latest step count. Update steps to be 2000. Print steps to the console. Then print "Good job! You're well on your way to your daily goal."
 
