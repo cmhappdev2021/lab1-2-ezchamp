f11b329a0c0281362ac25fa0afbfdab92a1a3f07
ezchamp <emile269@gmail.com>
2020-09-03 18:39:39 +0000

    Added multiple variables

--- main.swift
+++ main.swift
@@ -91,13 +91,17 @@ print("I made this a variable because a human's heart rate fluctuates")
 
 // Now go back and add a line after each constant or variable declaration. On those lines, print a statement explaining why you chose to declare the piece of information as a constant or variable.
 
+//Completed :)
 
 // Type Safety
 // Declare two variables, one called firstDecimal and one called secondDecimal. Both should have decimal values. Look at both of their types by holding Option and clicking on the variable name.
 
+var firstDecimal = 2.3
+var secondDecimal = 2.4
 
 // Declare a variable called trueOrFalse and give it a boolean value. Try to assign it to firstDecimal like so: firstDecimal = trueOrFalse. Does it compile? Print a statement to the console explaining why not, and remove the line of code that will not compile.
 
+var trueOrFalse = true
 
 // Declare a variable and give it a string value. Then try to assign it to firstDecimal. Does it compile? Print a statement to the console explaining why not, and remove the line of code that will not compile.
 
