df1148052cc0fd8090a9c0c21d3198dc0c2bed30
ezchamp <emile269@gmail.com>
2020-09-03 18:42:02 +0000

    Added print statment.

--- main.swift
+++ main.swift
@@ -102,6 +102,8 @@ var secondDecimal = 2.4
 // Declare a variable called trueOrFalse and give it a boolean value. Try to assign it to firstDecimal like so: firstDecimal = trueOrFalse. Does it compile? Print a statement to the console explaining why not, and remove the line of code that will not compile.
 
 var trueOrFalse = true
+//firstDecimal = trueOrFalse
+print("The code does not compile because once a type is set, it cannot be changed.")
 
 // Declare a variable and give it a string value. Then try to assign it to firstDecimal. Does it compile? Print a statement to the console explaining why not, and remove the line of code that will not compile.
 
