a3f4c1b6ceb6534533693e588e08541599009aeb
ezchamp <emile269@gmail.com>
2020-09-04 18:35:03 +0000

    Finished Project

--- main.swift
+++ main.swift
@@ -150,3 +150,5 @@ var percentCompleted:Double = 0
 // Imagine that partway through the day a user has taken 3,467 steps out of the 10,000 step goal. This means he/she is 34.67% of the way to his/her goal. Assign 34.67 to percentCompleted. Does the code compile? Go back and explicity assign a type to percentCompleted that will allow the code to compile.
 
 percentCompleted = 34.67
+
+//completed project
