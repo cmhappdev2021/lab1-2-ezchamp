dbb6dac3108eeec986f70c036b3794243aeca7c8
ezchamp <emile269@gmail.com>
2020-09-04 18:20:57 +0000

    Added variable and set it as a double.

--- main.swift
+++ main.swift
@@ -133,12 +133,15 @@ var nameOfType:String
 
 // Now assign a value to name, and print it to the console.
 
+nameOfType = "Obama2020"
 
 // Declare a variable called distanceTraveled and set it to 0. Do not give it an explicit type.
 
+var distanceTraveled:Double = 0
 
 // Now assign a value of 54.3 to distanceTraveled. Does the code compile? Go back and set an explicit type on distanceTraveled so the code will compile.
 
+distanceTraveled = 54.3
 
 // You decide that your fitness tracking app should show the user what percentage of his/her goal has been achieved so far today. Declare a variable called percentCompleted and set it to 0. Do not explicity assign it a type.
 
